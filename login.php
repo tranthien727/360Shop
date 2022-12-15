290621e905a88c0c988bbc84659ce1aba1589310
tranthien727 <tran.thien727@gmail.com>
2022-12-15 19:03:34 +0700

    add dangky login logout

---
+++ login.php
@@ -0,0 +1,97 @@
+<?php ob_start();
+ include 'partials/header.php';
+ 
+//Khai báo utf-8 để hiển thị được tiếng việt
+header('Content-Type: text/html; charset=UTF-8');
+ 
+//Xử lý đăng nhập
+if (isset($_POST['submit'])) 
+{
+     
+    //Lấy dữ liệu nhập vào
+    $email = ($_POST['txtEmail']);
+    $password = ($_POST['txtPassword']);
+     
+    //Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
+    if (!$email || !$password) {
+        echo "Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+
+    // mã hóa pasword
+    $password = md5($password);
+     
+   //Kiểm tra tên đăng nhập có tồn tại không
+   $query = "SELECT * FROM nguoidung WHERE Email='$email'";
+   $result=mysqli_query($conn,$query);
+   if (mysqli_num_rows($result) == 0) {
+       echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+       exit;
+   }
+
+   //Lấy mật khẩu trong database ra
+   $row = mysqli_fetch_array($result);
+   $Email = $row['Email'];
+   $Hinhanh = $row['Hinhanh'];
+   $Hoten = $row['Hoten'];
+   $MaNguoidung = $row['MaNguoidung'];
+   $Quyen=$row['IDQuyen'];
+    
+   //So sánh 2 mật khẩu có trùng khớp hay không
+   if ($password != $row['Matkhau']) {
+       echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+       exit;
+   }
+    
+   //Lưu session
+   $_SESSION['Hinhanh'] = $Hinhanh;
+   $_SESSION['Hoten'] = $Hoten;
+   $_SESSION['MaNguoidung'] = $MaNguoidung;
+   $_SESSION['Email'] = $Email;
+   if($Quyen==2){
+        header("Location: Admin/sanpham.php");
+        die();
+   }
+   else{
+        header("Location: home.php");
+        die();
+   }
+}
+?>
+
+<section class="login">
+    <div class="container">
+    <form action='' method='POST'>
+        <table cellpadding='0' cellspacing='0' border='0' align="center">
+            <tr>
+                <td colspan="2"><h1>Đăng nhập</h1></td>
+            </tr>
+            <tr>
+                <td>
+                    Tên đăng nhập :
+                </td>
+                <td>
+                    <input type='text' name='txtEmail' size="60"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Mật khẩu :
+                </td>
+                <td>
+                    <input type='password' name='txtPassword' />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <button type="submit" class="signupbtn" name="submit"> Đăng nhập</button>
+                    <button type="text" class="signupbtn"><a class="dk" href='dangky.php' title='Đăng ký'>Đăng ký</a></button>
+                </td>
+            </tr>
+        </table>
+    </form>
+    </div>
+</section>        
+<?php include 'partials/footer.php';
+ob_end_flush();  ?>
--- partials/header.php
+++ partials/header.php
@@ -249,6 +249,74 @@
             height:100px;
         }
         /* footer */
+        /* dangky login*/
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
+        .dangky{
+            font-family: Arial, Helvetica, sans-serif;
+            padding: 100px;
+        }
+        .login{
+            font-family: Arial, Helvetica, sans-serif;
+            padding: 100px;
+        }
+        h1, td{
+            color: DodgerBlue;
+            font-weight: bold;
+        }
+        .login input[type=text], input[type=password] {
+            padding: 15px;
+            margin: 5px 0 22px 0;
+            display: inline-block;
+            border: none;
+            width:100%;
+            resize: vertical;
+            padding:15px;
+            border-radius:15px;
+            border:0;
+            box-shadow:4px 4px 10px rgba(0,0,0,0.2);
+        }
+        .login input[type=text]:focus, input[type=password]:focus {
+            outline: none;
+        }
+        .dangky input[type=text], input[type=password] {
+            padding: 15px;
+            margin: 5px 0 22px 0;
+            display: inline-block;
+            border: none;
+            width:100%;
+            resize: vertical;
+            padding:15px;
+            border-radius:15px;
+            border:0;
+            box-shadow:4px 4px 10px rgba(0,0,0,0.2);
+        }
+        .dangky input[type=text]:focus, input[type=password]:focus {
+            outline: none;
+        }
+        .signupbtn {
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+            opacity: 0.9;
+        }
+        button:hover {
+            opacity:1;
+        }
+        .signupbtn {
+            width: 30%;
+            border-radius:15px;
+            border:0;
+            box-shadow:4px 4px 10px rgba(0,0,0,0.2);
+        }
+        .dk{
+            text-decoration: none;
+            color:#f2f2f2;
+        }
+        /* dangky login*/
     </style>
 </head>
 <body>
@@ -261,9 +329,22 @@
             <a  href="cart-view.php">Giỏ hàng <i class="fa fa-cart-plus"> [<?php echo count($cart); ?>]</i></a>
         </div>
         <div class="d-flex">
-            <?php 
+        <?php 
+                if (isset($_SESSION['Hinhanh']) && $_SESSION['Hinhanh']){
+                    echo '<ul class="navbar-nav me-auto">
+                    <li class="nav-item dropdown">
+                        <a class="navbar-brand" href="#" role="button" data-bs-toggle="dropdown"><img class="rounded-pill" style="width:30px;" src="HinhanhSP/'.$_SESSION["Hinhanh"].'"></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>';
+                }
+                else{
                     echo '<a href="login.php">Đăng nhập</a>
                             <a href="dangky.php">Đăng ký</a>';
+                }
             ?>
         </div>
         </div> 
